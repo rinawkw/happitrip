adbf6a3e05be8c11d063e646f2895d3e47788a90
Rina Wijaya <rinawkw@gmail.com>
2018-07-14 18:47:11 +0800

    layouts

---
+++ resources/views/layouts/audio.blade.php
@@ -0,0 +1,4 @@
+<!-- START PRELOADS -->
+<audio id="audio-alert" src="{{asset('assets/audio/alert.mp3')}}" preload="auto"></audio>
+<audio id="audio-fail" src="{{asset('assets/audio/fail.mp3')}}" preload="auto"></audio>
+<!-- END PRELOADS -->
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>        
+    @include('layouts.header')                               
+    </head>
+    <body>
+        <!-- START PAGE CONTAINER -->
+        <div class="page-container page-navigation-top-fixed">
+            
+            @include('layouts.sidebar')
+            
+            <!-- PAGE CONTENT -->
+            <div class="page-content">
+                
+                @include('layouts.navbar')
+
+                <!-- START BREADCRUMB -->
+                <ul class="breadcrumb">
+                    <li><a href="#">Home</a></li>                    
+                    <li class="active">Dashboard</li>
+                </ul>
+                <!-- END BREADCRUMB -->                       
+                
+                <!-- PAGE CONTENT WRAPPER -->
+                <div class="page-content-wrap">
+                    
+                    @yield('content')
+                    
+                </div>
+                <!-- END PAGE CONTENT WRAPPER -->                                
+            </div>            
+            <!-- END PAGE CONTENT -->
+        </div>
+        <!-- END PAGE CONTAINER -->
+
+        <!-- MESSAGE BOX-->
+        <div class="message-box animated fadeIn" data-sound="alert" id="mb-signout">
+            <div class="mb-container">
+                <div class="mb-middle">
+                    <div class="mb-title"><span class="fa fa-sign-out"></span> Log <strong>Out</strong> ?</div>
+                    <div class="mb-content">
+                        <p>Are you sure you want to log out?</p>                    
+                        <p>Press No if youwant to continue work. Press Yes to logout current user.</p>
+                    </div>
+                    <div class="mb-footer">
+                        <div class="pull-right">
+                            <a href="pages-login.html" class="btn btn-success btn-lg">Yes</a>
+                            <button class="btn btn-default btn-lg mb-control-close">No</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END MESSAGE BOX-->
+
+        @include('layouts.audio')                  
+        
+        @include('layouts.js')   
+    </body>
+</html>
+
+
+
+
+
+
